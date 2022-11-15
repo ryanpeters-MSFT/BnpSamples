756dadc5e2e24444eac03e7048628ed05ff5114e
Ryan Peters <ryanpeters@microsoft.com>
2022-11-15 08:12:28 -0500

    additional food seeds

--- EntityFrameworkSandbox/Migrations/20221115131133_adjust food seeding.Designer.cs
+++ EntityFrameworkSandbox/Migrations/20221115131133_adjust food seeding.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EntityFrameworkSandbox;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkSandbox.Migrations
 {
     [DbContext(typeof(FoodContext))]
-    partial class FoodContextModelSnapshot : ModelSnapshot
+    [Migration("20221115131133_adjust food seeding")]
+    partial class adjustfoodseeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
