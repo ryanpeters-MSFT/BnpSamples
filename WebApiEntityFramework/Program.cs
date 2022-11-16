5d435410cf286a1443f51cfaffc3d0e8e45b3384
Ryan Peters <ryanpeters@microsoft.com>
2022-11-16 11:12:38 -0500

    add AddXmlSerializerFormatters

--- WebApiEntityFramework/Program.cs
+++ WebApiEntityFramework/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<DogsContext>(options =>
 });
 
 builder.Services.AddScoped<IDogRepository, EfDogRepository>();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlSerializerFormatters();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
